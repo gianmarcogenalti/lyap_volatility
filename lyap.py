f16de62cf3e6ded76b20c3a6d64b793949cfdadb
Xenog <62154647+Xenog@users.noreply.github.com>
2020-11-08 16:22:56 +0100

    update2

--- lyap.py
+++ lyap.py
@@ -8,7 +8,7 @@ def der_evaluation(x, func):
          y = func(x_c)
     return g.gradient(y, x_c)
 
-def jacobian_mat(q, x, alpha_1, beta_0, beta_1, act_function):
+def jacobian_mat(q, m, x, alpha_1, beta_0, beta_1, act_function):
     J = np.zeros((m,m))
     i,j = np.indices(J.shape)
     J[i-1==j] = 1
@@ -17,12 +17,12 @@ def jacobian_mat(q, x, alpha_1, beta_0, beta_1, act_function):
         J[0,u] = sum([alpha_1[n]*der_evaluation(z[j], act_function)*beta_1[u,j] for j in range(q)])
     return J
 
-def lyapunov_coeff(q, x, alpha_0, alpha_1, beta_0, beta_1, act_function):
+def lyapunov_coeff(T, q, m, x, alpha_0, alpha_1, beta_0, beta_1, act_function):
     M=int(T**(2/3))
     Tm=np.identity(m)
-    for i in range(M,10,-1):
+    for t in range(M,10,-1):
         xm = x[(t-1):-1:(t-m)]
-        J=jacobian_mat(q, xm, alpha_1, beta_0, beta_1, act_function)
+        J=jacobian_mat(q, m, xm, alpha_1, beta_0, beta_1, act_function)
         Tm=np.dot(Tm,J)
     U0=np.zeros((m,1))
     U0[0]=1
